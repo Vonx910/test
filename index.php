603256fd9f32a2fde5256c2a983e7e848fd23766
Vonx910 <sookt9@gmail.com>
2021-10-19 22:09:22 +0800

    a

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Test 123</title>
+<title>Page Title</title>
 </head>
 <body>
 
 <h1>This is 1212</h1>
-<button>all</button>
 <p>This is a paragraph.</p>
 
 </body>
